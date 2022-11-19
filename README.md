d514d241ff9c4417b1d36a1e0ccd38ade00c3fe9
NgYujinBenjamin <benjaminng.2017@sis.smu.edu.sg>
2022-11-19 10:34:05 +0800

    prettier and lint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # carpark_frontend
 This is a frontend web application project that is utilizing Next.js framework that will retrieve data from the carpark backend application
+
+https://developers.google.com/codelabs/maps-platform/webgl#0
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const IndexPage = (): JSX.Element => (
-        <div>Boilerplate code for carpark frontend development</div>
-    )
+const IndexPage = (): JSX.Element => <div>Boilerplate code for carpark frontend development</div>;
 
 export default IndexPage;
